7d5ea391a88008743cf222b2ab9714f2223d3b98
minsu6862 <chl6863@gmail.com>
2025-07-16 17:51:43 +0900

    변수테스트

--- Java_WorkingSpace/javaTest_20250716/src/chapter03/Hello.java
+++ Java_WorkingSpace/javaTest_20250716/src/chapter03/Hello.java
@@ -5,6 +5,11 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+		
+		byte math; //byte정수 변수 선언
+		
+		math = 10;
+		System.out.println(math);
 	}
 
 }
