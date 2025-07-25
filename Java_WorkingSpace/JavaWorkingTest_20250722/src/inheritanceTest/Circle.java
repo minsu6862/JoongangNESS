ba20625f47a00748ec304f9630220f4c5180f06e
minsu6862 <chl6863@gmail.com>
2025-07-25 09:25:47 +0900

    수정3

--- Java_WorkingSpace/JavaWorkingTest_20250722/src/inheritanceTest/Circle.java
+++ Java_WorkingSpace/JavaWorkingTest_20250722/src/inheritanceTest/Circle.java
@@ -3,8 +3,9 @@ package inheritanceTest;
 public class Circle extends Shape {
 	static final double PI = 3.14;
 	
-	public double shapeArea(double radius) {
-		sum = PI * radius * radius;
-		return sum;
-	}
+    public double shapeArea(double width, double height) {
+        double radius = width * 0.5;
+        sum = PI * radius * radius;
+        return sum;
+    }
 }
