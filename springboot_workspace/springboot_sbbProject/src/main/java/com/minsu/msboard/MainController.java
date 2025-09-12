e5a9f93c77b1d96eec9caf057c75b69dc24906b9
minsu6862 <chl6863@gmail.com>
2025-09-12 15:07:26 +0900

    oracle db서버 배포완료

--- springboot_workspace/springboot_sbbProject/src/main/java/com/minsu/msboard/MainController.java
+++ springboot_workspace/springboot_sbbProject/src/main/java/com/minsu/msboard/MainController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+	//로컬용 루트
+	//@GetMapping(value = "/")
+	
+	//클라우드용 루트
 	@GetMapping(value = "/")
 	public String root() {
 		
--- springboot_workspace/springboot_sbbProject/src/main/java/com/minsu/msboard/SpringbootSbbProjectApplication.java
+++ springboot_workspace/springboot_sbbProject/src/main/java/com/minsu/msboard/SpringbootSbbProjectApplication.java
@@ -2,11 +2,18 @@ package com.minsu.msboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootSbbProjectApplication {
+public class SpringbootSbbProjectApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringbootSbbProjectApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSbbProjectApplication.class, args);
 	}
