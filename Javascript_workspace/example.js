615ef9014ae423b76e4ef1681d066d0e85e27b99
minsu6862 <chl6863@gmail.com>
2025-09-18 17:42:58 +0900

    js연습

---
+++ Javascript_workspace/example.js
@@ -0,0 +1,8 @@
+document.write("helloWorld");
+
+var str1 = "kor";
+var str2 = 'eng';
+var num1 = 100;
+var num2 = Number("100");   //문자 100 -> 숫자100
+var tag1 = "<h1> String </h1>";
+var bool1 = Boolean(null);  //false와 null이 아닌 값은 모두 true
