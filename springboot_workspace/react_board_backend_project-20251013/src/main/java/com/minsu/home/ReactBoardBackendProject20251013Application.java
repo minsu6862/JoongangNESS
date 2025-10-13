461d455e8e26ebad0ad2f907f6602e67e44ed279
minsu6862 <chl6863@gmail.com>
2025-10-13 11:12:07 +0900

    리엑트 백엔드 연동 보드 프로젝트

---
+++ springboot_workspace/react_board_backend_project-20251013/src/main/java/com/minsu/home/ReactBoardBackendProject20251013Application.java
@@ -0,0 +1,13 @@
+package com.minsu.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBoardBackendProject20251013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBoardBackendProject20251013Application.class, args);
+	}
+
+}
